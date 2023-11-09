38a57fd7ced5c76688ca5b88bb4aa44c134ee0fd
nuoxu0728 <xu.nuo1@northeastern.edu>
2023-11-09 11:07:57 -0800

    no message

--- backend/models/models.py
+++ backend/models/models.py
@@ -7,14 +7,13 @@ class Users(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True)
-    username = Column(String, unique=True)
+    
     first_name = Column(String)
     surname = Column(String)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
     role = Column(String)
-    phone_number = Column(String)
+  
 
 class Transactions(Base):
     __tablename__ = 'transactions'
--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -1,22 +1,21 @@
+
+import secrets
 from datetime import timedelta, datetime
-
-from fastapi import APIRouter, Depends, HTTPException
-from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
-from starlette import status
-
-from models.models import *
-from passlib.context import CryptContext
-from db.database import SessionLocal
-from typing import Annotated, Any
 from sqlalchemy.orm import Session
 from jose import jwt, JWTError
+
+from models.models import Users
+from db.database import SessionLocal
+from passlib.context import CryptContext
 
 router = APIRouter(prefix='/auth', tags=['auth'])
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 oauth2_bearer = OAuth2PasswordBearer(tokenUrl='token')
-SECRET_KEY = "your_secret_key"
+SECRET_KEY = secrets.token_urlsafe(32)
 ALGORITHM = "HS256"
 
 def get_db():
@@ -29,60 +28,79 @@ def get_db():
 db_dependency = Depends(get_db)
 
 class UserRegistration(BaseModel):
-    email: str
-    username: str
     first_name: str
     last_name: str
     password: str
-    user_role: str
+    role: str
 
 class TokenData(BaseModel):
     access_token: str
     token_type: str
 
-@router.post("/")
+@router.post("/register", response_model=TokenData)
 async def register_user(user_data: UserRegistration, db: Session = db_dependency):
+    existing_user = db.query(Users).filter(
+        Users.first_name == user_data.first_name,
+        Users.surname == user_data.last_name
+    ).first()
+    
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User with given name already exists."
+        )
+    
     user = Users(
-        email=user_data.email,
-        username=user_data.username,
         first_name=user_data.first_name,
         surname=user_data.last_name,
-        role=user_data.user_role,
+        role=user_data.role,
         hashed_password=bcrypt_context.hash(user_data.password),
         is_active=True
     )
     db.add(user)
     db.commit()
+    db.refresh(user)
+    
+    access_token_expires = timedelta(minutes=30)
+    access_token = create_access_token(
+        data={"sub": f"{user.first_name} {user.surname}", "id": user.id},
+        expires_delta=access_token_expires
+    )
+    
+    return {"access_token": access_token, "token_type": "bearer"}
 
 @router.post("/token", response_model=TokenData)
-async def generate_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = db_dependency):
-    user = verify_credentials(form_data.username, form_data.password, db)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = db_dependency):
+    user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
-    token_expires = timedelta(minutes=30)
-    token = create_jwt_token(user.username, user.id, user.role, token_expires)
-    return {"access_token": token, "token_type": "bearer"}
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect login details")
+    
+    access_token_expires = timedelta(minutes=30)
+    access_token = create_access_token(
+        data={"sub": f"{user.first_name} {user.surname}", "id": user.id},
+        expires_delta=access_token_expires
+    )
+    
+    return {"access_token": access_token, "token_type": "bearer"}
 
-@router.post("/login")
-async def login(user_id: int, db: Session = Depends(get_db)):
-    user = db.query(Users).filter(Users.id == user_id).first()
+def authenticate_user(first_name: str, last_name: str, password: str, db: Session):
+    user = db.query(Users).filter(
+        Users.first_name == first_name,
+        Users.surname == last_name
+    ).first()
+    
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
-    
-    
-    token_expires = timedelta(minutes=30)
-    token = create_jwt_token(user.username, user.id, user.role, token_expires)
-    return {"access_token": token, "token_type": "bearer"}
-
-def verify_credentials(username: str, password: str, db: Session) -> Users:
-    user = db.query(Users).filter(Users.username == username).first()
-    if not user or not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
         return False
     return user
 
-def create_jwt_token(username: str, user_id: int, role: str, expires_delta: timedelta) -> str:
-    data_to_encode = {"sub": username, "id": user_id, "role": role}
-    expiration = datetime.utcnow() + expires_delta
-    data_to_encode.update({"exp": expiration})
-    encoded_jwt = jwt.encode(data_to_encode, 'YOUR_SECRET_KEY', algorithm='HS256')
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
